5cfdb765ac1263af94efe8b7606989d715619b41
Dshellz <davidsanchez38550@gmail.com>
2025-02-02 14:54:18 +0100

    Contact Part
    server nodejs

---
+++ server.js
@@ -0,0 +1,39 @@
+// filepath: /d:/Projet Perso/portfolio-website/portfolio-website/Portfolio-Cyber/server.js
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.post('/send-email', (req, res) => {
+  const { name, email, message } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'clientgestion9@gmail.com',
+      pass: 'aSrtkgi87!@'
+    }
+  });
+
+  const mailOptions = {
+    from: email,
+    to: 'your-email@gmail.com',
+    subject: `Contact form submission from ${name}`,
+    text: message
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return res.status(500).send(error.toString());
+    }
+    res.status(200).send('Email sent: ' + info.response);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
